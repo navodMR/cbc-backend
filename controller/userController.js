a4efdf41ca597cba0f4361ec3c2fd61f4a62e767
navodMR <navodwwe456@gmail.com>
2024-11-29 13:13:55 +0530

    add userlogin function and routes

--- controller/userController.js
+++ controller/userController.js
@@ -16,4 +16,35 @@ user.save().then(()=>{res.json({
 
 }
 
-export{createUser};
+function loginUser(req,res){
+
+    User.find({email : req.body.email}).then(
+        (users)=>{
+            if (users.length == 0){
+                res.json({
+                    message:"user not found"
+                })
+                
+            
+            }else{
+
+                const user = users[0]
+                const isPasswordCorrect = bcrypt.compareSync(req.body.password,user.password)
+
+                if (isPasswordCorrect){
+                    res.json({
+                        message:"user loged in"
+                    })
+                }else{
+                    res.json({
+                        message:"invalid password"
+                    })
+                }
+            }
+        }
+    )
+
+
+}
+
+export{createUser , loginUser};
